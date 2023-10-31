4ed4ea4e13260b9d295e27a3820d374c8e0bae62
sodaler <misha_lector87@mail.ru>
2023-10-31 15:43:35 +0400

    CategoryController test. first chapter

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ * @extends Factory
  */
 class CategoryFactory extends Factory
 {
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->text(40),
+            'title' => $this->faker->title,
             'description' => $this->faker->text,
             'image' => $this->faker->imageUrl,
             'image_alt' => $this->faker->slug,
